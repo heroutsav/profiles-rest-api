d4142b0b04bf7af7b5a10ae7b3491facef6a8732
Utsav <heroutsav@gmail.com>
2023-10-26 17:27:58 +0530

    Added user login

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include
-from profiles_api import views
+
 from rest_framework.routers import DefaultRouter
+
+from profiles_api import views
+
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
 
+
 urlpatterns = [
-path('hello-view/', views.HelloApiView.as_view()),
-path('', include(router.urls)),
+    path('hello-view/', views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
+    path('', include(router.urls))
 ]
